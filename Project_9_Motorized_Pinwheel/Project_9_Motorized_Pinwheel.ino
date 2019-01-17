7cce42655083cabb69291ea6fd9d2872f43ea980
Jan <janaarondomingo@gmail.com>
2019-01-16 22:26:59 -0800

    Project 9 Completed

---
+++ Project_9_Motorized_Pinwheel/Project_9_Motorized_Pinwheel.ino
@@ -0,0 +1,22 @@
+//Project 9 Motorized Pinwheel
+//Get the Arduino to spin a colorful pinwheel using a motor
+
+const int switchPin = 2;
+const int motorPin = 9;
+int switchState = 0;
+
+void setup() {
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(switchPin);
+
+  if (switchState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+  }
+}
