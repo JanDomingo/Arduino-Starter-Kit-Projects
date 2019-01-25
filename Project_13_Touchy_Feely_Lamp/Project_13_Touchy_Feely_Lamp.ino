9d182c32a5bfa232a77c37dba91c0dcf4fe95351
Jan <janaarondomingo@gmail.com>
2019-01-24 20:02:41 -0800

    Project 13 Completed

---
+++ Project_13_Touchy_Feely_Lamp/Project_13_Touchy_Feely_Lamp.ino
@@ -0,0 +1,27 @@
+//Project 13 Touchy Feely Lamp
+//Create a lamp that turns a light on and off when you touch a piece of conductive material
+
+#include <CapacitiveSensor.h>
+CapacitiveSensor capSensor = CapacitiveSensor (4,2);
+
+int threshold = 1000;
+const int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  long sensorValue = capSensor.capacitiveSensor(30);
+  Serial.println(sensorValue);
+
+  if(sensorValue > threshold) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(10);
+
+}
